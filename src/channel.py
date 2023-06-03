5e28a4e7c3c109282bf7daef664461e3aa2ef747
miwnaya <mikgerasimov@gmail.com>
2023-06-03 12:56:30 +0300

    Minor changes

--- src/channel.py
+++ src/channel.py
@@ -11,22 +11,20 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_list = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
-        self.__channelid= self.channel_list["items"][0]["id"]
+        self.__channelid = self.channel_list["items"][0]["id"]
         self.title = self.channel_list["items"][0]["snippet"]["title"]
         self.video_count = self.channel_list["items"][0]["statistics"]["videoCount"]
         self.url = "https://www.youtube.com/channel/" + self.__channelid
         self. subscriber_count = self.channel_list["items"][0]["statistics"]["subscriberCount"]
         self.view_count = self.channel_list["items"][0]["statistics"]["viewCount"]
 
-
     @property
     def channel_id(self):
-        return self.__channel_id
+        return self.__channelid
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.channel_list, indent=2, ensure_ascii=False))
-
 
     @classmethod
     def get_service(cls):
@@ -36,6 +34,4 @@ class Channel:
     def to_json(self, filename):
         """Записывает аттрибуты класса в json файл"""
         with open(filename, 'w') as outfile:
-            #json.dump(self.channel_id, outfile)
             json.dump(self.__dict__, outfile)
-
