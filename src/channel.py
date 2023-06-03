5d8aeb755e4751abe68c6d7d67ee23dd08641697
miwnaya <mikgerasimov@gmail.com>
2023-06-03 12:55:11 +0300

    Added channel attributes Added get_service method Added to_json method

--- src/channel.py
+++ src/channel.py
@@ -3,7 +3,6 @@ import os
 from googleapiclient.discovery import build
 
 
-
 class Channel:
     """Класс для ютуб-канала"""
     api_key: str = os.getenv('API_KEY')
@@ -12,7 +11,31 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_list = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.__channelid= self.channel_list["items"][0]["id"]
+        self.title = self.channel_list["items"][0]["snippet"]["title"]
+        self.video_count = self.channel_list["items"][0]["statistics"]["videoCount"]
+        self.url = "https://www.youtube.com/channel/" + self.__channelid
+        self. subscriber_count = self.channel_list["items"][0]["statistics"]["subscriberCount"]
+        self.view_count = self.channel_list["items"][0]["statistics"]["viewCount"]
+
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.channel_list, indent=2, ensure_ascii=False))
+
+
+    @classmethod
+    def get_service(cls):
+        """Возвращает объект для работы с API вне класса"""
+        return cls.youtube
+
+    def to_json(self, filename):
+        """Записывает аттрибуты класса в json файл"""
+        with open(filename, 'w') as outfile:
+            #json.dump(self.channel_id, outfile)
+            json.dump(self.__dict__, outfile)
+
